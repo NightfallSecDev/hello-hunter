5f10003a039641d188257b6f4e8df9e1848c7359
NightfallSecDev <m1000tech007@gmail.com>
2024-12-10 16:45:36 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,40 @@
+const toggleButton = document.getElementById('toggle-btn')
+const sidebar = document.getElementById('sidebar')
+
+function toggleSidebar(){
+  sidebar.classList.toggle('close')
+  toggleButton.classList.toggle('rotate')
+
+  closeAllSubMenus()
+}
+
+function toggleSubMenu(button){
+
+  if(!button.nextElementSibling.classList.contains('show')){
+    closeAllSubMenus()
+  }
+
+  button.nextElementSibling.classList.toggle('show')
+  button.classList.toggle('rotate')
+
+  if(sidebar.classList.contains('close')){
+    sidebar.classList.toggle('close')
+    toggleButton.classList.toggle('rotate')
+  }
+}
+
+function closeAllSubMenus(){
+  Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
+    ul.classList.remove('show')
+    ul.previousElementSibling.classList.remove('rotate')
+  })
+}
+
+function copyCode() {
+  const codeSnippet = document.getElementById("code-snippet").innerText;
+  navigator.clipboard.writeText(codeSnippet).then(() => {
+    alert("Code copied to clipboard!");
+  }).catch(err => {
+    alert("Failed to copy code: " + err);
+  });
+}
